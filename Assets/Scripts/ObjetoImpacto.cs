07e883502db72abbd4341aeda8ed867d7ef8da84
Jesus Reyna <jerenav16@gmail.com>
2026-02-15 16:13:44 -0600

    Se agrega función GetComponent dentro del método OnCollisionEnter

--- Assets/Scripts/ObjetoImpacto.cs
+++ Assets/Scripts/ObjetoImpacto.cs
@@ -7,6 +7,12 @@ public class ObjetoImpacto : MonoBehaviour
     // es chocado por algo como el jugador
     private void OnCollisionEnter(Collision collision)
     {
+        // Con esta fiunción se obtiene el componente de un objeto par
+        // modificar sus propiedades.
+        // Entonces cuando haya colision las paredes u obstaculos
+        // cambiarán a color magenta
+        GetComponent<MeshRenderer>().material.color = Color.magenta;
+        
         Debug.Log("Algo me golpeó!");
     }
 }
