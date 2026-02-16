75f4ef724fa2f424352a59785040ffaad17e550d
Jesus Reyna <jerenav16@gmail.com>
2026-02-15 21:18:40 -0600

    Se usa casheo de referencia en el MeshRenderer y Rigidbody en Dropper.cs

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] float tiempoDeEspera = 3.0f;
+    MeshRenderer miMeshRenderer;
+    Rigidbody miRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        miMeshRenderer = GetComponent<MeshRenderer>();
+        miRigidbody = GetComponent<Rigidbody>();
+
+        miMeshRenderer.enabled = false;
+        miRigidbody.useGravity = false;
+
     }
 
     // Update is called once per frame
@@ -14,7 +21,8 @@ public class Dropper : MonoBehaviour
     {
         if (Time.time > tiempoDeEspera)
         {
-            Debug.Log("Cuidado Abajo!");
+            miMeshRenderer.enabled = true;
+            miRigidbody.useGravity = true;
         }
     }
 }
