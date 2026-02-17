c68cd9871670b2bd11c3b367af4a267d0bb7f7ab
Jesus Reyna <jerenav16@gmail.com>
2026-02-17 10:31:30 -0600

    Se agrega el script TriggerProyectil.cs que evalua el evento Trigger del objeto Trigger Volume cuando el objeto jugador colisiona con él. Entonces se activan los objetos de juego Proyectil que fueron previamente desactivados para que actuen.

---
+++ Assets/Scripts/TriggerProyectil.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TriggerProyectil : MonoBehaviour
+{
+    // Se hace referencia a los objetos de juego Proyectil
+    // Por medio del Inspector gracias SerializeField
+    // en el campo proyectil.
+
+    // Cuando el jugador colisiona en el objeto trigger
+    // este compara si la etiqueta es Player y
+    // acitva al proyectil para realice sus instrucciones
+
+    // Nota: Primero se debe desactivar el objeto del 
+    // Juego para luego activarlo en el script.
+    // Si no hará sus instrucciones independientemente
+    // del trigger
+    [SerializeField] GameObject proyectil;
+    [SerializeField] GameObject proyectil2;
+    [SerializeField] GameObject proyectil3;
+    [SerializeField] GameObject proyectil4;
+    [SerializeField] GameObject proyectil5;
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            proyectil.SetActive(true);
+            proyectil2.SetActive(true);
+            proyectil3.SetActive(true);
+            proyectil4.SetActive(true);
+            proyectil5.SetActive(true);
+        }    
+    }
+}
